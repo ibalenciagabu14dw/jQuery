c4efc85735c2959befae7316830a57785846d060
ibalenciagabu14dw <ibalenciagabu14dw@ikzubirimanteo.com>
2015-11-26 10:38:43 +0000

    fotos metidas

--- Ariketak/bases_de_Datos/acordeon/datos.php
+++ Ariketak/bases_de_Datos/acordeon/datos.php
@@ -1,5 +1,5 @@
 <?php
-	$pdo = new PDO('mysql:host=localhost;dbname=ejercicios','ibalenciagabu14d','');
+	$pdo = new PDO('mysql:host=localhost;dbname=ejercicios','root','');
 	$statement = $pdo->query('SELECT * FROM personajes');
 	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($result);
---
+++ Ariketak/bases_de_Datos/acordeon/funciones.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$('#formulario').submit(function(event) {
+		console.log("submit");
+		event.preventDefault();
+
+		$('#resp').html('');
+
+		$.ajax({
+			url: 'datos.php',
+			type: 'post',
+			dataType: 'json',
+			success: function (data) {
+				var resp = "";
+				for (var i = 0; i < data.length; i++) {
+					resp += "<h3>"+data[i].id+" "+data[i].nombre+"</h3>";
+					resp += "<div><img src="+data[i].foto+"/></div>";
+				};
+				$('#resp').html(resp);
+
+				//Si tiene clase de accordion
+				if ($('#resp').hasClass('ui-accordion')) {
+					//destruye el accordion
+                	$('#resp').accordion('destroy');
+                	//Vuelve a crear el accordion
+                	$('#resp').accordion({
+						collapsible:true,
+						active:false
+					});
+				}else{
+					//si no, crea el accordion
+					$('#resp').accordion({
+						collapsible:true,
+						active:false
+					});
+				}//if
+			}//success
+		});//ajax
+	});//submit
+});
