36fe2aba9f62a8b74f2df11845c1028200e1ddea
ibalenciagabu14dw <ibalenciagabu14dw@ikzubirimanteo.com>
2015-11-30 09:00:39 +0000

    ejercicios.sql

--- Ariketak/bases_de_Datos/ejercicios.sql
+++ Ariketak/bases_de_Datos/ejercicios.sql
@@ -3,11 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2015 a las 11:24:42
+-- Tiempo de generación: 30-11-2015 a las 08:50:35
 -- Versión del servidor: 5.5.46-0ubuntu0.14.04.2
 -- Versión de PHP: 5.5.9-1ubuntu4.14
-CREATE DATABASE IF NOT EXISTS ejercicios;
-use ejercicios;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ejercicios`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulos`
+--
+
+CREATE TABLE IF NOT EXISTS `articulos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(30) NOT NULL,
+  `dolares` decimal(6,2) NOT NULL,
+  `euros` decimal(6,2) NOT NULL,
+  `libras` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `articulos`
+--
+
+INSERT INTO `articulos` (`id`, `descripcion`, `dolares`, `euros`, `libras`) VALUES
+(1, 'Pendrive 16GB', '23.00', '21.74', '15.31'),
+(2, 'Disco serial ATA', '45.00', '42.53', '29.95'),
+(3, 'Pantalla SONY 28 pulgadas', '154.00', '145.53', '102.49'),
+(4, 'HP laser color', '245.00', '231.53', '163.06');
 
 -- --------------------------------------------------------
 
@@ -40,11 +63,11 @@ CREATE TABLE IF NOT EXISTS `personajes` (
 --
 
 INSERT INTO `personajes` (`id`, `nombre`, `imagen`) VALUES
-(2, 'homer', 'Ariketak/bases_de_datos/acordeon/img/homer.jpg'),
-(3, 'marge', 'Ariketak/bases_de_datos/acordeon/img/marge.jpg'),
-(4, 'bart', 'Ariketak/bases_de_datos/acordeon/img/bart.jpg'),
-(5, 'lisa', 'Ariketak/bases_de_datos/acordeon/img/lisa.jpg'),
-(6, 'maggi', 'Ariketak/bases_de_datos/acordeon/img/maggi.jpg');
+(2, 'homer', 'fotos/homer.jpg'),
+(3, 'marge', 'fotos/marge.jpg'),
+(4, 'bart', 'fotos/bart.jpg'),
+(5, 'lisa', 'fotos/lisa.jpg'),
+(6, 'maggie', 'fotos/maggie.jpg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
